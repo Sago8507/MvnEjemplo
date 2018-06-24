446ac4d18c12edbb4239da0f668b80f30a6495bd
omar salinas <titochuloteama@gmail.com>
2018-06-24 00:08:04 -0500

    Initial commit

---
+++ src/main/java/mx/sago/java/App.java
@@ -0,0 +1,14 @@
+package mx.sago.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("Nueva linea");
+        System.out.println( "Hello World!" );
+    }
+}

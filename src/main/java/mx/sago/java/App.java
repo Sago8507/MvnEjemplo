90f5a0c5f10f0ff424e5de6d44e81d83ca7e96eb
omar salinas <titochuloteama@gmail.com>
2018-06-24 00:47:04 -0500

    cambio de nivel de compilacion
    Se cambio la manera de tomar version de compilacion
    Se agregaron librerias de spring y mysql
    Se ignoran archivos de eclipse

--- src/main/java/mx/sago/java/App.java
+++ src/main/java/mx/sago/java/App.java
@@ -4,11 +4,9 @@ package mx.sago.java;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	System.out.println("Nueva linea");
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Nueva linea");
+		System.out.println("Hello World!");
+	}
 }

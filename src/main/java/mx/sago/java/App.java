f50640ad79aff25735591a41c07f64b0265040d2
omar salinas <titochuloteama@gmail.com>
2018-07-15 17:36:04 -0500

    variables de datos
    se pasaron las variables a de customer

--- src/main/java/mx/sago/java/App.java
+++ src/main/java/mx/sago/java/App.java
@@ -14,11 +14,17 @@ public class App {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
 	    	 
-	        CustomerDAO customerDAO = (CustomerDAO) context.getBean("customerDAO");
-	        Customer customer = new Customer(3, "santiago",6);
+	        
+		int id = 4;
+		String name = "Isaac";
+		int age = 6;
+		
+		
+		CustomerDAO customerDAO = (CustomerDAO) context.getBean("customerDAO");
+	        Customer customer = new Customer(id, name, age);
 	        customerDAO.insert(customer);
 	    	
-	        Customer customer1 = customerDAO.findByCustomerId(3);
+	        Customer customer1 = customerDAO.findByCustomerId(id);
 	        System.out.println(customer1);
 	}
 }
